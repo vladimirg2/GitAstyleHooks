7c4a8c71deb8ae9f594d7b3b5d41f6c0fb1b3d70
vladimirg2 <vladimir.g777@gmail.com>
2015-06-29 00:38:24 -0400

    Need to run find instead of ls to find all the files and cleanup.

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # GitAstyleHooks
 A few hooks to make Artistic Style part of git. Prevent un-styled files from beeing commited, etc. 
 
-To install:
+To install the pre-commit hook, execute the following command in the root directory of the repo:
 
 ln -s ../../pre-commit.py .git/hooks/pre-commit
 
-Edit 'defaultOptions.txt' to point it to your custom astyle options file.
+To install the update (server-side) hook:
+
+ln -s ../../update.py .git/hooks/update
+
+
+
+
+
+For both hooks, edit the path to to the 'defaultOptions.txt' file, to be valid on your machine. 
 
  
 
--- update.py
+++ update.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 #Git update hook to reject any push with unstyled files.
+#Written for Python 2.7.6
+
 
 ##############################################################
 # THIS IS A WORK IN PROGRESS! It's 1 AM, I NEED SLEEP!
@@ -13,7 +15,7 @@ import sys
 import subprocess
 import shlex
 
-TEMPDIR='.updateScratchSpace8975309753075375349894'
+TEMPDIR='.updateScratchSpace8909830803489f890293'
 
 #The update hook recieves #refname sha1-old sha1-new as arguments:   
 
@@ -33,21 +35,42 @@ def main():
     commandString = "git archive "
     commandString += new_commit + " | " 
     commandString += "tar -x -C  " + TEMPDIR
-    print "commandString: " + commandString 
+     
     
     os.system(commandString)
     os.chdir(TEMPDIR)
+
+    proc1 = subprocess.Popen('pwd', stdout=subprocess.PIPE)
+    fullPath,err = proc1.communicate()
+
+    commandString = "ls "#W.I.P: find . -name .c and .cpp file | to astyle check astyle's output. 
+            
     
-    commandString = "ls -l "#W.I.P: find every .c and .cpp file | to astyle check astyle's output. 
-    print "commandString = " + commandString
     proc1 = subprocess.Popen(shlex.split(commandString), stdout=subprocess.PIPE)
-    fileList,err=proc1.communicate()
-    print (fileList)
-    print (err)
+    rawOutput,err=proc1.communicate()
+    fileList = rawOutput.splitlines()
+    abortCommit = False
+    
+    for fileName in fileList:
+        fileName = str.strip(fileName)
+        if fileName.endswith('.h') or fileName.endswith('.cpp') or fileName.endswith('.c'):
+            commandString = 'astyle --options=/home/vld/StylingOptionsFIleLocation/defaultOptions.txt %s/' % fullPath.rstrip('\n') 
+            commandString += fileName
+            
+            proc2 = subprocess.Popen(shlex.split(commandString), stdout=subprocess.PIPE)
+            astyleOutput,err = proc2.communicate();
+            parsedOutput = astyleOutput.splitlines()
+            for lines in parsedOutput:
+                if lines.find('Formatted') >= 0:
+                    print 'Invalid Style Format within file: %s\n' % fileName
+                    abortCommit = True
 
-    os.system('rm -rf %s' % TEMPDIR) 
+    os.system('rm -rf %s' % TEMPDIR)                
+    if abortCommit:
+        print "Aborting commit!\nStyle then push again.\n"
+        exit(1)
+    exit(1)#Should be 0 once development and testing is done. 
 
-    exit(1)#W.I.P.
 
 
 if __name__ == "__main__":
